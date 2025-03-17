517bac75afd67ab33d2a0488c4cceecd397d4456
tomasz <tomasz@com>
2025-03-17 04:53:16 +0100

    box

---
+++ codewars/box.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+width = 12
+height = 15
+depth = 17
+
+obj = width * height * depth
+
+powierzchnia = 2 *(width*height + height* depth + width* depth)
+
+
+# Example usage
+
+print(f"Surface Area:   {powierzchnia},\nVolume:   {obj}")
